2c8e8b6a526546e8f1e244ce47db12d162b3d9bc
Asgeir <asgeir@cluda.com>
2022-11-11 20:30:39 +0100

    Passport base

--- src/Passport/Passport.sol
+++ src/Passport/Passport.sol
@@ -1,39 +1,112 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.9;
 
-import {ERC721OnlyOwnerTransferable} from "../ERC721OnlyOwnerTransferable/ERC721OnlyOwnerTransferable.sol";
+import {ERC721} from "../../lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import {AccessControl} from "../../lib/openzeppelin-contracts/contracts/access/AccessControl.sol";
+import {EIP712} from "../../lib/openzeppelin-contracts/contracts/utils/cryptography/draft-EIP712.sol";
+import {ERC721Votes} from "../../lib/openzeppelin-contracts/contracts/token/ERC721/extensions/draft-ERC721Votes.sol";
 import {Counters} from "../../lib/openzeppelin-contracts/contracts/utils/Counters.sol";
 
-contract Passport is ERC721OnlyOwnerTransferable {
+contract Passport is ERC721, AccessControl, EIP712, ERC721Votes {
     using Counters for Counters.Counter;
 
+    error Disabled();
+
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant TRANSFERER_ROLE = keccak256("TRANSFERER_ROLE");
     Counters.Counter private _tokenIdCounter;
+    string private baseURI;
 
     constructor(
         string memory name_,
         string memory symbol_,
         string memory baseURI_
-    ) ERC721OnlyOwnerTransferable(name_, symbol_, baseURI_) {}
+    ) ERC721(name_, symbol_) EIP712(name_, "1") {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
+        _grantRole(TRANSFERER_ROLE, msg.sender);
+        baseURI = baseURI_;
+    }
 
-    // OnlyOwner functions
-    function safeMint(
-        address to,
-        string memory /* uri */
-    ) public {
+    function _baseURI() internal pure override returns (string memory) {
+        return baseURI;
+    }
+
+    function updateBaseURI(string memory newBaseURI)
+        public
+        onlyRole(DEFAULT_ADMIN_ROLE)
+    {
+        baseURI = newBaseURI;
+    }
+
+    function safeMint(address to) public onlyRole(MINTER_ROLE) {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
     }
 
-    function updateBaseURI(string memory newBaseURI) public onlyOwner {
-        _baseURI = newBaseURI;
+    // Configure the approval functionality to return expected values
+    function approve(
+        address, /* to */
+        uint256 /* tokenId */
+    ) public pure override {
+        revert Disabled();
     }
 
-    function updateName(string memory newName) public onlyOwner {
-        _name = newName;
+    function setApprovalForAll(
+        address, /* operator */
+        bool /* approved */
+    ) public pure override {
+        revert Disabled();
     }
 
-    function updateSymbol(string memory newSymbol) public onlyOwner {
-        _symbol = newSymbol;
+    function getApproved(uint256 tokenId)
+        public
+        view
+        override
+        returns (address)
+    {
+        _requireMinted(tokenId);
+
+        return address(0);
+    }
+
+    // Make sure the token is only transferable by the TRANSFERRER_ROLE
+
+    function isApprovedForAll(
+        address, /* owner */
+        address operator /* operator */
+    ) public pure override returns (bool) {
+        return hasRole(TRANSFERER_ROLE, operator);
+    }
+
+    function _isApprovedOrOwner(address spender, uint256 tokenId)
+        internal
+        view
+        override
+        returns (bool)
+    {
+        _requireMinted(tokenId);
+        return isApprovedForAll(address(0), spender);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _afterTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId,
+        uint256 batchSize
+    ) internal override(ERC721, ERC721Votes) {
+        super._afterTokenTransfer(from, to, tokenId, batchSize);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, AccessControl)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 }
--- src/Passport/Passport.test.sol
+++ src/Passport/Passport.test.sol
@@ -5,28 +5,14 @@ import "../../lib/forge-std/src/Test.sol";
 import {Passport} from "./Passport.sol";
 
 contract PassportTest is Test {
-    address public deployer = 0xb4c79daB8f259C7Aee6E5b2Aa729821864227e84;
-    address public alice = 0xD028d504316FEc029CFa36bdc3A8f053F6E5a6e4;
-    address public bob = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
-    Passport public passport;
+    Passport public token;
 
     function setUp() public {
-        passport = new Passport("Passport", "PAS", "");
+        token = new Passport("A Token", "AT", "");
     }
 
     function testSetupParameters() public {
-        assertEq(passport.name(), "Passport");
-        assertEq(passport.symbol(), "PAS");
-    }
-
-    function testMint() public {
-        passport.safeMint(alice, "");
-        assertEq(passport.balanceOf(alice), 1);
-    }
-
-    function testOnlyOwnerCanMint() public {
-        vm.expectRevert("Ownable: caller is not the owner");
-        vm.prank(bob);
-        passport.safeMint(alice, "");
+        assertEq(token.name(), "A Token");
+        assertEq(token.symbol(), "AT");
     }
 }
